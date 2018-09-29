ee64daca0dcf653f97e6fff55532b61ef5d371ad
icezeros <icezeros@163.com>
2018-09-29 18:48:41 +0800

    ethErc20 model

---
+++ app/model/ethErc20.js
@@ -0,0 +1,22 @@
+module.exports = app => {
+  const mongoose = app.mongoose;
+
+  const EthErc20Schema = new mongoose.Schema(
+    {
+      _id: String,
+      address: String,
+      name: String,
+      symbol: String,
+      decimals: Number,
+      icon: String,
+    },
+    {
+      typeKey: '$type',
+      timestamps: {
+        createdAt: 'created',
+        updatedAt: 'updated',
+      },
+    }
+  );
+  return mongoose.model('eth_erc20', EthErc20Schema);
+};
