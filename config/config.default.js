d4cdc12e778d21e7099ece959e0e52e966881511
Phil <qft.gtr@gmail.com>
2021-01-07 01:50:50 +0800

    Update config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -10,14 +10,12 @@ module.exports = appInfo => {
 
   // redis缓存block过期时长
   config.redisBlockExpire = process.env.REDIS_BLOCK_EXPIRE
-  // 区块回溯次数
-  config.backtrackIteration = Number(process.env.BACKTRACK_ITERATION)
-  // 通过web3获取区块失败重试次数
-  config.getBlockIterationTimes = process.env.GETBLOCK_ITERATION_TIMES
-  // 确认交易回溯次数
-  config.confirmBacktrackIteration = process.env.CONFIRM_BACKTRACK_ITERATION
-
   config.redisTxExpire = process.env.REDIS_TX_EXPIRE
+
+  config.backtrackIteration = Number(process.env.BACKTRACK_ITERATION) // 区块回溯次数
+  config.confirmBacktrackIteration = Number(process.env.CONFIRM_BACKTRACK_ITERATION) // 确认交易回溯次数
+
+  config.getBlockIterationTimes = process.env.GETBLOCK_ITERATION_TIMES // 通过web3获取区块失败重试次数
 
   // add your config here
   config.middleware = []
