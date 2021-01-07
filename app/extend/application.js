d247be22ff3a2fd181ed24fb0c7527436825f70a
Phil <qft.gtr@gmail.com>
2021-01-07 13:21:05 +0800

    Update application.js

--- app/extend/application.js
+++ app/extend/application.js
@@ -8,11 +8,13 @@
 
 const Web3 = require('web3')
 
-const web3 = new Web3(process.env.WEB3_HTTP_URL)
+const urls = process.env.WEB3_HTTP_URLS.split(',')
+const n = urls.length
+const web3s = urls.map(url => new Web3(url))
 
 module.exports = {
   get web3 () {
-    return web3
+    return web3s[Math.floor(Math.random() * n)]
   },
   async sleep (duration = 500) {
     return new Promise(resolve => setTimeout(resolve, duration))
